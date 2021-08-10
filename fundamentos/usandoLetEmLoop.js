301964389212d31ad81744984a03bd53fd72b017
EverttonFernandes <evertonoliveirafernandes@outlook.com>
2021-08-09 22:39:44 -0300

    Usando let em loop

---
+++ fundamentos/usandoLetEmLoop.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+console.log('i =', i) // no uso do let, o i não fica mais definido, sendo visível apenas dentro do bloco de código no qual foi/está sendo usado
