d775cc962b8cf923c125ae73e7be7a5e34a76c1c
EverttonFernandes <evertonoliveirafernandes@outlook.com>
2021-08-09 22:29:28 -0300

    Primeiro exemplo de declaração de variaveis com var

---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,21 @@
+
+{ 
+    { 
+        { 
+            { 
+                var sera = 'Será???' // em js variaveis definidas como var, podem ser visiveis fora do seu escopo (Variavel global)
+                console.log(sera)
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste() {
+    var local = 123 // porém neste caso a variavel só pode ser visível dentro da função
+    console.log(local)
+}
+
+teste()
+console.log(local)
